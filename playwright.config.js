f45b2769ed5a5a518158a6e50869b16b3d8ed6f5
shrikantkhemkar1187-sketch <shirkantkhemkar1187@gmail.com>
2026-03-31 15:52:48 +0530

    Added the files after npx playwright test --shard=1/4 to 4/4

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+['dot'],
+['list'],
+['allure-playwright'],
+['blob'],
+['json', {outputFile: 'reporter.json'}]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
