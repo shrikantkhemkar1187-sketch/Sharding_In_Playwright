225b5f655fd12ce2acb4e3310968109f4e7842fa
shrikantkhemkar1187-sketch <shirkantkhemkar1187@gmail.com>
2026-04-01 10:09:05 +0530

    update TestSharding file

--- tests/TestSharding.spec.js
+++ tests/TestSharding.spec.js
@@ -4,5 +4,40 @@ test('TC for google and Hello world', async ({page})=>{
 
     await page.goto("https://www.google.com/")
 
+        await page.goto("https://www.hyrtutorials.com/")
+
+
+            await page.goto("https://www.facebook.com/")
+
+
+                await page.goto("https://www.w3schools.com/")
+
+
+                    await page.goto("https://www.saucedemo.com/")
+
+
+                        await page.goto("https://mvnrepository.com/")
+
+
+                            await page.goto("https://www.redbus.in/")
+
+
+                                await page.goto("https://chatgpt.com/")
+
+
+                                    await page.goto("https://www.redbus.in/railways?utm_source=google&utm_medium=search&utm_campaign=IN-redRail-Search-Generic&utm_term=train&device=c&utm_matchtype=p&utm_network=g&utm_placement=&utm_content=767873207701&utm_Adposition=&utm_location=9040229&utm_Sitelink=}&utm_Audience=kwd-13289063&utm_campaignid=22860482859&utm_adgroupid=178709597970&utm_adgroup=&gad_source=1&gad_campaignid=22860482859&gbraid=0AAAAAoeJyA1va_5bOM2K6Bxih_1Seywi4&gclid=Cj0KCQjw4a3OBhCHARIsAChaqJOUdyCyB8eH4M5BWtaPeh55XDwfHSxNHwYEiyyh89I1011GCxGNVIkaAiRwEALw_wcB")
+
+
+                                        await page.goto("https://www.abhibus.com/?utm_source=google&utm_medium=cpc&utm_campaign=Generic_KWs_PanIndia_Aug29&gad_source=1&gad_campaignid=20149187670&gbraid=0AAAAABNk5GsiuwvyDMOcj5JdH2Zty5G6H&gclid=Cj0KCQjw4a3OBhCHARIsAChaqJMOFw9ETG1khUQ8AB2rSc63icEevqukHa2qxsPRO7mEpe51Mu9-2o8aAkQlEALw_wcB")
+
+
+
+                                            await page.goto("https://www.makemytrip.com/hotels/hotel-listing/?topHtlId=200708241650475894&locusId=CTNAG&locusType=city&city=CTNAG&country=IN&searchText=Nagpur&reference=hotel&type=city&cmp=SEM|D|DH|G|Hname|DH_HName_CTNAG_7-10K_DT|200708241650475894|R|&mf_source=google&mf_medium=cpc&mf_campaignid=20567655540&cid=Cj0KCQjw4a3OBhCHARIsAChaqJPnHj9-pOdzC8fPjtiQnGSTyxmr7EXCDMb4yqzWjLx_ukyI5sCCdN4aApkMEALw_wcB&pl=&kw=hotel%20centre%20point&adg=156598359569&aid=674898990458&campaign_type=search&device=c&mf_campaign={HARDCODE_CAMPAIGN_NAME}&gad_source=1&gad_campaignid=20567655540&gbraid=0AAAAAD5Az1TwX3lKTaoL4a0W4VP_7RIiE&gclid=Cj0KCQjw4a3OBhCHARIsAChaqJPnHj9-pOdzC8fPjtiQnGSTyxmr7EXCDMb4yqzWjLx_ukyI5sCCdN4aApkMEALw_wcB")
+
+
+
+                                                await page.goto("https://www.unionbankonline.bank.in/")
+
+
     console.log("Hello world!")
 })
